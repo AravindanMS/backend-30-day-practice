fc9bb96bd5a34257247d6236696a3173437428ec
AravindanMS <aravindms40@gmail.com>
2026-06-02 11:25:49 +0530

    Day 1: Add Express health and users endpoints

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express = require("express");
 const app =express();
 app.use(express.json());
 
+const users = [
+  {id: 1, name: "Ram", email: "ram@gmail.com", role:"admin"},
+  {id:2, name:"Lakshman", email:"laksh@gmail.com", role:"user"}
+]
+
 app.get("/health",(req,res)=>{
   return res.status(200).json({
     status:"Ok",
     message:"Server is running"
   })
+})
+
+app.get("/users",(req,res)=>{
+  return res.status(200).json(users)
 })
 
 const PORT = 3000;

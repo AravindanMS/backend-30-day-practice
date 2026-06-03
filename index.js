53203e65da19b595ef4b9dd3166c8b7ee4288bb5
AravindanMS <aravindms40@gmail.com>
2026-06-03 10:26:03 +0530

    Day 1: Add user detail route with 404 handling

--- index.js
+++ index.js
@@ -18,6 +18,18 @@ app.get("/users",(req,res)=>{
   return res.status(200).json(users)
 })
 
+app.get("/users/:id",(req,res)=>{
+  const id = Number(req.params.id);
+  const user = users.find(u => u.id === id);
+
+  if(!user){
+    return res.status(404).json({
+      message:"User not found"
+    })
+  }
+  return res.status(200).json(user);
+})
+
 const PORT = 3000;
 
 app.listen(PORT,()=>{
